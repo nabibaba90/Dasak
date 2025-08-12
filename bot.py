b3e2d250c05d1a0f867c3729b9833db92a9864ab
nabibaba90 <kolsuzpabg@gmail.com>
2025-08-12 20:48:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -316,7 +316,7 @@ apis = {
         "params": ["gsm"]
     },
 }
-@app.route("/keneviz/<api_name>")
+@app.route("/Keneviz/<api_name>")
 def api_proxy(api_name):
     if api_name not in apis:
         return "API bulunamadı", 404
